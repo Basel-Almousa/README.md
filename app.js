34c8c40467f5b3e5943e400401d54b59bbdf1288
baselalmusa <>
2021-06-02 18:10:26 +0000

    ,

---
+++ app.js
@@ -0,0 +1,15 @@
+var x = 1
+var y = x + 3
+console.log(y)
+
+alert(" Welcom to the world of travel ")
+var userAnswer = prompt("Are interested in an award?")
+var yes = 1 + "yes" 
+if (userAnswer =yes ) {
+  var q = prompt("How many continents in the world?")
+  if (q = 7) { alert("CONGRATULATION")
+    alert("YOU WON A TAVEL DOCUMENT TO PARIS")
+    alert("for more info contact us here: 123456")}
+    else {("ooops, try again later")}
+}
+else{ alert("Thanks for your time")}
