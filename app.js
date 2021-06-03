f1f7e893726568cc216ff09f663403da25b51312
baselalmusa <>
2021-06-03 19:15:56 +0000

    ,

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 alert(" Welcom to the world of travel ")
 var userAnswer = prompt("Are you interested in an award? type yes if you are interested?") 
 if (userAnswer == "yes") {
@@ -17,20 +14,4 @@ userinput = prompt("how many picture is in the webpage? isn't = 5 :P")
  for(var x = 0 ; x < userinput ; x++ ){
  document.write("<a>"+ "<Image src=s.jpg>" + "</a>")}
 
-=======
-var x = 1
-var y = x + 3
-console.log(y)
 
-alert(" Welcom to the world of travel ")
-var userAnswer = prompt("Are interested in an award?")
-var yes = 1 + "yes" 
-if (userAnswer =yes ) {
-  var q = prompt("How many continents in the world?")
-  if (q = 7) { alert("CONGRATULATION")
-    alert("YOU WON A TAVEL DOCUMENT TO PARIS")
-    alert("for more info contact us here: 123456")}
-    else {("ooops, try again later")}
-}
-else{ alert("Thanks for your time")}
->>>>>>> 34c8c40467f5b3e5943e400401d54b59bbdf1288
