79eeae4b72ba9113f4bd28ced2e673f139a5e67f
baselalmusa <>
2021-06-03 17:40:05 +0000

    ,

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+alert(" Welcom to the world of travel ")
+var userAnswer = prompt("Are you interested in an award? type yes if you are interested?") 
+if (userAnswer == "yes") {
+  var q = prompt("Are there 5 or 7 continents in the world?")
+  var y = 7; 
+  while(q == y){ alert("CONGRATULATION")
+    alert("YOU WON A TAVEL DOCUMENT TO PARIS")
+    alert("for more info contact us here: 123456")
+    break}
+
+}
+alert("And now answer the next question and we will directly send you the award!!!")
+userinput = prompt("how many picture is in the webpage? isn't = 5 :P") 
+ for(var x = 0 ; x < userinput ; x++ ){
+ document.write("<a>"+ "<Image src=s.jpg>" + "</a>")}
+
